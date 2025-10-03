90beb45d3f312315232fdcc8aee5c7094cb20a44
anadiasxavier <anadiasxavier07@gmail.com>
2025-10-03 09:28:33 -0300

    teste começo

---
+++ simuladoSaep/FrontEnd/Kanban/src/__test__/CadTarefa.test.jsx
@@ -0,0 +1,17 @@
+// import { render, screen, fireEvent, waitFor, getByLabelText } from '@testing-library/react';
+// import { describe, expect } from 'vitest';
+// import { CadUsuario } from '../Paginas/CadTarefa';
+
+// describe("Cadastro de tarefa", () =>{
+    
+//     it("A tela é exibida", ()=>{
+//         render(<CadTarefa/>);
+
+//         const nomeInput = screen.getAllByLabelText(/Nome/i);
+        
+
+
+
+//         expect(nomeInput).toBeTruthy();
+//     })
+// })
--- simuladoSaep/FrontEnd/Kanban/src/__test__/CadastroUsuario.test.jsx
+++ simuladoSaep/FrontEnd/Kanban/src/__test__/CadastroUsuario.test.jsx
@@ -6,6 +6,8 @@ import { CadUsuario } from '../Paginas/CadUsuario';
 //fireEvent: simula o que usuario pode fazer em tela
 //waitFor: espera o resultado do evento 
 
+//para rodar npm run test
+
 describe("Cadastro de usuario", () =>{
 
     it("A tela é exibida", ()=>{
@@ -18,5 +20,28 @@ describe("Cadastro de usuario", () =>{
         expect(nomeInput).toBeTruthy();
         expect(emailInput).toBeTruthy();
         expect(botao).toBeTruthy();
-        })
+        });
+
+        it("deve mostrar erros quando campos estiverem vazios", async () =>{
+            render(<CadUsuario/>);
+
+            fireEvent.click(screen.getByRole("button", {name:/Cadastrar/i}));
+
+            await waitFor(() =>{
+                expect(screen.getByText("Insira ao menos 1 caractere")).toBeTruthy();
+                expect(screen.getByText("Insira seu email")).toBeTruthy();
+            });
+        });
+
+        it("deve mostrar erro quando o email tiver formato inválido", async ()=>{
+            render(<CadUsuario/>);
+
+            fireEvent.input(screen.getByLabelText(/Nome/i), { target: { value: "Maria" } });
+            fireEvent.input(screen.getByLabelText(/E-mail/i), { target: { value: "maria.com" }
+        });
+
+        fireEvent.submit(screen.getByRole("form") || screen.getByRole("button", {name:/Cadastrar/i}));
+            await waitFor(() =>{
+                expect(screen.getByText(/Formato de email invalido/i)).toBeTruthy();
+            })
 })
